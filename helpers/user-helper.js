1ce7f4ea00ed05c89bab59dfec737992a078c5b5
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2023-11-13 21:21:31 +0300

    Update user-helper.js

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -156,8 +156,32 @@ module.exports = {
                 resolve(response)
             })
         })
-    }
+    },
+    getDatabByMonthAndEmployee: ( month, employeeId) => {
+        
+        return new Promise((resolve, reject) => {
+            const [year, monthNumber] = month.split('-');
+            const firstDayOfMonth = new Date(year, monthNumber - 1, 1);
+            const lastDayOfMonth = new Date(year, monthNumber, 0, 23, 59, 59, 999); // Set to the end of the last day of the month
+            db.get().collection('datasheet').find({
+                $and: [
+                    { date: { $gte: firstDayOfMonth } },
+                    { date: { $lte: lastDayOfMonth } },
+                    { employee_id: employeeId } // Adjust the field name based on your actual schema
+                ]
+            }).toArray()
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                reject(error);
+            });
+        });
+    },
+    
 
     
 }
+
 
