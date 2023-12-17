64a4a223e7c1dc3eb2cb76953b5cd8590f751ea3
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2023-12-17 16:04:56 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -171,7 +171,7 @@ router.get('/printdatasheet', (req, res) => {
 
         pdf.create(html, options).toStream((err, stream) => {
             if (err) {
-                return res.status(500).send(err);
+                console.log('error is here')
             }
 
             res.setHeader('Content-Type', 'application/pdf');
