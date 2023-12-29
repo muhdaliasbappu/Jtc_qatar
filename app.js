5db3caa3395924f1c74eb29a52d4e9f132ca39c3
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2023-12-29 22:17:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 
 var adminRouter = require('./routes/admin');
 var userRouter = require('./routes/users');
-var superRouter = require('./routes/superadmin')
 var hbs = require('express-handlebars');
 var app = express();
 var db = require('./config/connection');
@@ -48,7 +47,7 @@ app.use((req, res, next) => {
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
-app.use('/superadmin', superRouter);
+
 
 
 
