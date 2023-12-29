e19828ee3a0b158ba09d8ab0bd50dde0a544dec9
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2023-12-29 22:24:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,39 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
 
+
 var adminRouter = require('./routes/admin');
 var userRouter = require('./routes/users');
 var hbs = require('express-handlebars');
 var app = express();
 var db = require('./config/connection');
-
-const session = require('express-session');
-
+const { hasSubscribers } = require('diagnostics_channel');
+var session = require('express-session')
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/' },));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: "key", cookie: { maxAge: 6000000 } }));
-
-
-app.set('subdomain offset', 1);
-app.use((req, res, next) => {
-  const subdomain = req.subdomains[0];
- 
-  let dbName = "Jtcqatars";
-  if (subdomain) {
-    dbName = subdomain;
-    dbid = subdomain;
-  }
-  db.connect(dbName, (err) => {
-    if (err) {
-      console.error("Error connecting to the database:", err);
-    } else {
-      console.log(`Connected to database: ${dbName}`);
-    }
-    
-    next();
-  });
-});
-
+app.use(session({ secret: "key", cookie: { maxAge: 6000000 } }))
+db.connect((err) => {
+  if (err)
+    console.log("connection error" + err)
+  else console.log("database connected")
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
-
-
-
-
-// Rest of your code
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
