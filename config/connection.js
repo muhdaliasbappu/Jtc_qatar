78fd90db70f036b944f2f6aa5d63e6c6b8d743c3
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2023-12-29 22:23:52 +0300

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,35 +4,39 @@ const state = {
   db: null,
 };
 
+// mongodb connection string
 const url = "mongodb://127.0.0.1:27017";
+// database name
+const dbName = "Jtcqatar";
 
+// create a new mongodb client object
 const client = new MongoClient(url);
 
-const connect = async (dbName, cb) => {
+// function to establish mongodb connection
+const connect = async (cb) => {
   try {
+    // connecting to mongodb
     await client.connect();
+    // setting up database name to the connected client
     const db = client.db(dbName);
+    // setting up database name to the state
     state.db = db;
-    // Check if cb is a function before calling it
-    if (typeof cb === 'function') {
-      return cb();
-    } else {
-      console.error('Callback is not a function');
-    }
+    // callback after connected
+    return cb();
   } catch (err) {
-    // Check if cb is a function before calling it with the error
-    if (typeof cb === 'function') {
-      return cb(err);
-    } else {
-      console.error('Callback is not a function');
-    }
+    // callback when an error occurs
+    return cb(err);
   }
 };
 
+// function to get the database instance
 const get = () => state.db;
 
+// exporting functions
 module.exports = {
   connect,
   get,
 };
+
+
 
