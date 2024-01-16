bf4e213a603ee2692e855e1a0accbdb36631edad
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2024-01-16 23:52:12 +0300

    Update DayView.js

--- modules/DayView.js
+++ modules/DayView.js
@@ -5,6 +5,28 @@ module.exports = {
       const dayIndex = dateObj.getDay();
       const formattedDate = `${dateObj.getDate()}-${dateObj.getMonth() + 1}-${dateObj.getFullYear()} ${daysOfWeek[dayIndex]}`;
       return formattedDate;
-    }
+    },
+ getMonthAndYear: (dateString)=> {
+
+  const [year, month] = dateString.split("-");
+
+  const monthNumber = parseInt(month, 10);
+
+  // Validate that month is between 1 and 12
+  
+  if (monthNumber >= 1 && monthNumber <= 12) {
+    
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+
+    const uppercaseMonth = monthNames[monthNumber - 1].toUpperCase();
+    return `${uppercaseMonth} ${year}`;
+  } else {
+    
+    return "Invalid date format";
+  }
+}
   };
   
