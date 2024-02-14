d44a6d2d623ab1c8527e6176fb7aa73f846e6131
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2024-02-15 00:52:56 +0300

    Update user-helper.js

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -420,8 +420,7 @@ module.exports = {
                                 { date: { $lte: lastDayOfMonth } },
                                 { employee_id: id },
                                 { todaystatus: 'Paid Leave' },
-                             // { employeeType: employeeType }
-                                
+                                { employeeType: employeeType }
                                 
                             ]
                         },
@@ -432,8 +431,7 @@ module.exports = {
                                 { employee_id: id },
                                 { workinghour: '0' },
                                 { todaystatus: 'Working' },
-                                       //  { employeeType: employeeType }
-                              
+                                { employeeType: employeeType }
                               
                             ]
                         }
