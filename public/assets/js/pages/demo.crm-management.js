b69932cfa7741360426d57b0964b40045126c578
muhdaliasbappu <68806442+muhdaliasbappu@users.noreply.github.com>
2025-01-08 12:42:27 +0300

    Update demo.crm-management.js

--- public/assets/js/pages/demo.crm-management.js
+++ public/assets/js/pages/demo.crm-management.js
@@ -23,7 +23,7 @@ let apexChart = null; // Will hold the current ApexCharts instance
       "#39afd1", // Hired Staff(P)
       "#ffbc00", // Operation Cost
       "#ff6b6b", // Over Head Cost
-      "#727cf5"  // Total Cost
+      "#cbff43"  // Total Cost
     ];
 
     // If you have a custom color list in a data attribute:
